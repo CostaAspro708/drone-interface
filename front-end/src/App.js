c132f582545962f7144d10076b0e3f6770510b82
Costa Aspromourgos <n10464174@qut.edu.au>
2023-08-10 09:57:34 +1000

    [SETUP] Installed tailwind and configured the tailwind config file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,38 +1,22 @@
 import "./App.css";
-import HttpCall from "./components/HttpCall";
 import WebSocketCall from "./components/WebSocketCall";
 import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [socketInstance, setSocketInstance] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [buttonStatus, setButtonStatus] = useState(false);
-
-  const handleClick = () => {
-    if (buttonStatus === false) {
-      setButtonStatus(true);
-    } else {
-      setButtonStatus(false);
-    }
-  };
-
+  const [messages, setMessages] = useState([]);
+  const socket = io("localhost:5001/", {
+    transports: ["websocket"],
+    cors: {
+      origin: "http://localhost:3000/",
+    },
+  });
   useEffect(() => {
-    if (buttonStatus === true) {
-      const socket = io("localhost:5001/", {
-        transports: ["websocket"],
-        cors: {
-          origin: "http://localhost:3000/",
-        },
-      });
-
-      setSocketInstance(socket);
+      
 
       socket.on("connect", (data) => {
         console.log(data);
       });
-
-      setLoading(false);
 
       socket.on("disconnect", (data) => {
         console.log(data);
@@ -41,25 +25,23 @@ function App() {
       return function cleanup() {
         socket.disconnect();
       };
-    }
-  }, [buttonStatus]);
+    
+  }, []);
+  useEffect(() => {
+    socket.on("data", (data) => {
+      setMessages([...messages, data.data]);
+    });
+    return () => {
+      socket.off("data", () => {
+        console.log("data event was removed");
+      });
+    };
+  }, [socket, messages]);
 
   return (
     <div className="App">
-      <h1>React/Flask App + socket.io</h1>
-      <div className="line">
-        <HttpCall />
-      </div>
-      {!buttonStatus ? (
-        <button onClick={handleClick}>turn chat on</button>
-      ) : (
-        <>
-          <button onClick={handleClick}>turn chat off</button>
-          <div className="line">
-            {!loading && <WebSocketCall socket={socketInstance} />}
-          </div>
-        </>
-      )}
+      <h1 className="text-xl text-center">React/Flask App + socket.io</h1>
+
     </div>
   );
 }
